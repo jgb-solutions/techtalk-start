07c845ff3080e2157294862db7dddb50cf2cd1b4
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2025-01-14 14:54:14 -0500

    Updated footer link to use Link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,12 @@
 import { APP_NAME } from "~/utils/constants"
 import Container from "./Container"
+import { Link } from "@tanstack/react-router"
 
 export default function Footer() {
   return (
     <footer className="bg-black text-white">
       <Container className="flex items-center justify-between">
-        <a href="/"><img className="w-24 mb-4" src="/assets/images/techtalk-logo@4x.png" alt={APP_NAME} /></a>
+        <Link to="/"><img className="w-24 mb-4" src="/assets/images/techtalk-logo@4x.png" alt={APP_NAME} /></Link>
 
         <p className="font-medium">&copy; 2025</p>
       </Container>
