2a66b3e02d2c901c82c79b54e3983c91dffeec4c
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2025-01-24 01:09:43 -0500

    Added padding for titles

--- app/components/Episode.tsx
+++ app/components/Episode.tsx
@@ -17,12 +17,10 @@ export default function Episode({
         params={{ episodeId: episode.id }}
       >
         <img src={episode.cdnImageUrl} alt={episode.title} className="sm:mr-2 sm:w-72" />
-        <div className="flex-1 flex-col">
-          <h3 className="text-2xl p-4 sm:p-0">
-            <span className="font-thin">{formatTitle(episode.title)[0]}:</span><br />
-            <span className="font-medium">{formatTitle(episode.title)[1]}</span>
-          </h3>
-        </div>
+        <h3 className="text-2xl p-4 flex-1">
+          <span className="font-thin">{formatTitle(episode.title)[0]}:</span><br />
+          <span className="font-medium">{formatTitle(episode.title)[1]}</span>
+        </h3>
       </Link>
     </div>
   )
