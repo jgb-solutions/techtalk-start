dfa608f32e299daf2f20cab60701cffa2d4565c5
Jean Gerard Bousiquot <jgbneatdesign@gmail.com>
2025-01-25 10:52:20 -0500

    Update SEO twitter username

--- app/utils/seo.ts
+++ app/utils/seo.ts
@@ -1,11 +1,11 @@
-import { SITE_URL } from "./constants"
+import { SITE_URL, X_HANDLE } from "./constants"
 import getPhotonUrl from "./photon"
 
 export const seo = ({
   title,
   description,
   keywords,
-  image = getPhotonUrl({ src: `${SITE_URL}/assets/images/grunged-mockup.jpg`, width: 720 }),
+  image = getPhotonUrl({ src: `${SITE_URL}/assets/images/grunged-mockup.jpg` }),
 }: {
   title: string
   description?: string
@@ -18,8 +18,8 @@ export const seo = ({
     { name: 'keywords', content: keywords },
     { name: 'twitter:title', content: title },
     { name: 'twitter:description', content: description },
-    { name: 'twitter:creator', content: '@tannerlinsley' },
-    { name: 'twitter:site', content: '@tannerlinsley' },
+    { name: 'twitter:creator', content: `@${X_HANDLE}` },
+    { name: 'twitter:site', content: `@${X_HANDLE}` },
     { name: 'og:type', content: 'website' },
     { name: 'og:title', content: title },
     { name: 'og:description', content: description },
